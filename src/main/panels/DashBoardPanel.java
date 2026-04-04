0abd8ef0725af7bd25cd72db17024b32e854f36b
rynez <s2010188@usls.edu.ph>
2026-04-05 02:15:35 +0800

    Added ButtonPanel

--- src/main/panels/DashBoardPanel.java
+++ src/main/panels/DashBoardPanel.java
@@ -8,7 +8,9 @@ import java.util.HashMap;
 
 public class DashBoardPanel extends JPanel {
 
+    private MainFrame mainFrame;
 
+    private JPanel buttonPanel;
 
 
     public DashBoardPanel(MainFrame mainFrame) {
@@ -17,6 +19,20 @@ public class DashBoardPanel extends JPanel {
 
         setLayout(new BorderLayout());
         setBackground(new Color(240,240,240));
+
+        ButtonPanel();
+
+    }
+
+    private void ButtonPanel(){
+
+        buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT,15,15));
+        buttonPanel.setBackground(new Color(240,240,240));
+
+        buttonPanel.add(Button("BDO", "bdo"));
+        buttonPanel.add(Button("Maya", "maya"));
+        buttonPanel.add();
+
 
 
     }
