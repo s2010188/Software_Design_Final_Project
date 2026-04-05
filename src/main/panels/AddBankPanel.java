963be864e8f4dec839dd300cacff7e180f4f0696
aifosNS <s2010060@usls.edu.ph>
2026-04-06 00:05:57 +0800

    d

--- src/main/panels/AddBankPanel.java
+++ src/main/panels/AddBankPanel.java
@@ -1,7 +1,9 @@
 package main.panels;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.File;
 
 public class AddBankPanel extends JPanel {
     private  MainFrame mainFrame;
@@ -11,7 +13,7 @@ public class AddBankPanel extends JPanel {
     private ImageIcon selectedLogo;
 
 
-    public AddBankPanel(MainFrame mainFrame){
+    public AddBankPanel(MainFrame mainFrame) {
         this.mainFrame = mainFrame;
 
         setLayout(new BorderLayout());
@@ -22,11 +24,11 @@ public class AddBankPanel extends JPanel {
         JPanel cardPanel = new JPanel();
         cardPanel.setLayout(new BoxLayout(cardPanel, BoxLayout.Y_AXIS));
         cardPanel.setBackground(Color.WHITE);
-        cardPanel.setPreferredSize(new Dimension(380,420));
-        cardPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(new Color(200,200,200)), BorderFactory.createEmptyBorder(30,30,30,30)));
+        cardPanel.setPreferredSize(new Dimension(380, 420));
+        cardPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(new Color(200, 200, 200)), BorderFactory.createEmptyBorder(30, 30, 30, 30)));
 
 
-        JLabel title= new JLabel("+ Add New Bank");
+        JLabel title = new JLabel("+ Add New Bank");
         title.setFont(new Font("Arial", Font.BOLD, 22));
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
@@ -35,17 +37,16 @@ public class AddBankPanel extends JPanel {
         banklabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         bankNameField = new JTextField();
-        bankNameField.setMaximumSize(new Dimension(320,35));
-        bankNameField.setPreferredSize(new Dimension(320,35));
+        bankNameField.setMaximumSize(new Dimension(320, 35));
+        bankNameField.setPreferredSize(new Dimension(320, 35));
 
 
         //logo prev
-        logoPreview= new JLabel("No Logo",SwingConstants.CENTER);
-        logoPreview.setPreferredSize(new Dimension(160,70));
-        logoPreview.setMaximumSize(new Dimension(160,70));
+        logoPreview = new JLabel("No Logo", SwingConstants.CENTER);
+        logoPreview.setPreferredSize(new Dimension(160, 70));
+        logoPreview.setMaximumSize(new Dimension(160, 70));
         logoPreview.setBorder(BorderFactory.createLineBorder(Color.GRAY));
         logoPreview.setAlignmentX(Component.CENTER_ALIGNMENT);
-
 
 
         //buttons
@@ -56,16 +57,16 @@ public class AddBankPanel extends JPanel {
         chooseBtn.setBorderPainted(false);
         chooseBtn.setOpaque(true);
 
-        JButton addBtn= new JButton("Add Bank");
-        JButton backBtn= new JButton("Back");
+        JButton addBtn = new JButton("Add Bank");
+        JButton backBtn = new JButton("Back");
 
         chooseBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
         addBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
         backBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        chooseBtn.setMaximumSize(new Dimension(180,35));
+        chooseBtn.setMaximumSize(new Dimension(180, 35));
         addBtn.setMaximumSize(new Dimension(180, 35));
-        backBtn.setMaximumSize(new Dimension(100,28));
+        backBtn.setMaximumSize(new Dimension(100, 28));
 
         addBtn.setBackground(Color.decode("#5D765E"));
         addBtn.setForeground(Color.WHITE);
@@ -76,6 +77,82 @@ public class AddBankPanel extends JPanel {
         chooseBtn.addActionListener(e -> chooseLogo());
         addBtn.addActionListener(e -> addBank());
         backBtn.addActionListener(e -> mainFrame.showPanel("dashboard"));
+
+
+        cardPanel.add(title);
+        cardPanel.add(Box.createVerticalStrut(25));
+
+        cardPanel.add(banklabel);
+        cardPanel.add(Box.createVerticalStrut(8));
+
+        cardPanel.add(bankNameField);
+        cardPanel.add(Box.createVerticalStrut(20));
+
+        cardPanel.add(logoPreview);
+        cardPanel.add(Box.createVerticalStrut(15));
+
+        cardPanel.add(chooseBtn);
+        cardPanel.add(Box.createVerticalStrut(20));
+
+        cardPanel.add(addBtn);
+        cardPanel.add(Box.createVerticalStrut(15));
+
+        cardPanel.add(backBtn);
+
+
+        JPanel wrap = new JPanel(new GridBagLayout());
+        wrap.setBackground(Color.decode("#F0F0F0"));
+        wrap.add(cardPanel);
+
+
+        add(wrap, BorderLayout.CENTER);
+
+    }
+
+    private void chooseLogo() {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        chooser.setAcceptAllFileFilterUsed(true);
+        chooser.setFileHidingEnabled(false);
+
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Images (PNG, JPG, JPEG)",
+                "png", "jpg", "jpeg");
+
+        chooser.setFileFilter(filter);
+
+        int result = chooser.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = chooser.getSelectedFile();
+            ImageIcon icon = new ImageIcon(file.getAbsolutePath());
+
+            Image img = icon.getImage().getScaledInstance(140, 50, Image.SCALE_SMOOTH);
+
+            selectedLogo = new ImageIcon(img);
+
+            logoPreview.setIcon(selectedLogo);
+            logoPreview.setText("");
+
+
+        }
+    }
+
+    private void addBank(){
+        String bank = bankNameField.getText().trim();
+        if(bank.isEmpty()){
+            JOptionPane.showMessageDialog(this, "Enter bank name" );
+            return;
+
+        }
+        mainFrame.addNewbank(bank, selectedLogo);
+
+        bankNameField.setText("");
+        logoPreview.setIcon(null);
+        logoPreview.setText("No Logo");
+
+        mainFrame.showPanel("dashboard");
+
+
+
 
 
 
