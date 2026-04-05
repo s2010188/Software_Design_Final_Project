09db558e25828eea2e04b4488dc8e55ffcffe015
balquin <s2010682@usls.edu.ph>
2026-04-05 21:45:18 +0800

    edit

--- src/main/app/MainFrame.java
+++ src/main/app/MainFrame.java
@@ -10,6 +10,19 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class MainFrame extends JFrame {
+
+
+    public DashBoardPanel getDashboard(){
+        return dashboardPanel;
+    }
+
+
+    private CardLayout cardLayout;
+    private JPanel mainContainer;
+
+    private HashMap<String, JPanel> panels = new HashMap<>();
+    private DashBoardPanel dashboardPanel;
+
     public static void main(String[] args) {
 
     }
