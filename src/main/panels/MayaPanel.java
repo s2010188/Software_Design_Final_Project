6a1140b91b95f6daf6f8defb19a4ac9075c603be
aifosNS <s2010060@usls.edu.ph>
2026-04-05 22:28:15 +0800

    2nd part center n somehow savings

--- src/main/panels/MayaPanel.java
+++ src/main/panels/MayaPanel.java
@@ -32,6 +32,43 @@ public class MayaPanel extends JPanel {
         Image img = icon.getImage().getScaledInstance(70,70,Image.SCALE_SMOOTH);
         logo.setIcon(new ImageIcon(img));
 
+        JLabel title = new JLabel("Maya Bank");
+        title.setFont(new Font("Arial", Font.BOLD, 18));
+
+        JPanel leftHeader = new JPanel(new FlowLayout(FlowLayout.LEFT,10,0));
+        leftHeader.setOpaque(false);
+        leftHeader.add(logo);
+        leftHeader.add(title);
+
+        header.add(leftHeader, BorderLayout.WEST);
+
+        JPanel cardPanel= new JPanel(new BorderLayout());
+        cardPanel.setBackground(Color.WHITE);
+        cardPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(new Color(200,200,200),1), BorderFactory.createEmptyBorder(15,15,15,15)));
+
+
+
+
+        //Center
+        JPanel center = new JPanel();
+        center.setLayout(new BoxLayout(center,BoxLayout.Y_AXIS));
+        center.setBackground(Color.WHITE);
+
+
+        //The deafult savings
+        JPanel savingsPanel= createPanel("Savings");
+        savingsPanel.setBackground(Color.decode("#F5F5F5"));
+
+        savingsPanel.setPreferredSize(new Dimension(0,110));
+        savingsPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE,110));
+
+        savingsLabel= new JLabel("PHP 0.00");
+        savingsLabel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        JButton editSavings = createEditButton();
+        
+
+
 
     }
 
