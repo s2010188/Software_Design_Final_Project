ca4e87211ca6a53bb12ff2908d40742d7d8d31cf
reign <s2010083@usls.edu.ph>
2026-04-07 01:17:16 +0800

    partial V2

--- src/main/panels/BDOPanel.java
+++ src/main/panels/BDOPanel.java
@@ -37,5 +37,36 @@ public class BDOPanel extends JPanel {
             logo.setFont(new Font("Arial", Font.BOLD, 24));
             logo.setForeground(new Color(0, 51, 102));
         }
+
+        JLabel title = new JLabel("BDO UNIBANK");
+        title.setFont(new Font("Arial", Font.BOLD, 18));
+
+        JPanel leftHeaderEL = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
+        leftHeaderEL.setOpaque(false);
+        leftHeaderEL.add(logo);
+        leftHeaderEL.add(title);
+
+        headerEL.add(leftHeaderEL, BorderLayout.WEST);
+
+        JPanel eCardPanel = new JPanel(new BorderLayout());
+        eCardPanel.setBackground(Color.WHITE);
+        eCardPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(new Color(200, 200, 200), 1), BorderFactory.createEmptyBorder(15, 15, 15, 15)));
+
+        JPanel center = new JPanel();
+        center.setLayout(new BoxLayout(center, BoxLayout.Y_AXIS));
+        center.setBackground(Color.WHITE);
+
+        JPanel savingsPanelEL = createPanel("Savings");
+        savingsPanelEL.setBackground(new Color(245, 245,245));
+
+        savingsPanelEL.setPreferredSize(new Dimension(0, 110));
+        savingsPanelEL.setMaximumSize(new Dimension(Integer.MAX_VALUE, 110));
+
+        savingsLabel = new JLabel("₱ 0.00");
+        savingsLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        savingsLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        JButton editSavings = createEditButton();
+
     }
 }
