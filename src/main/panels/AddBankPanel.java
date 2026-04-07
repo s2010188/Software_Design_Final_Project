8219d8d3ce1a499cb72ad46c00f997c9e84f337a
aifosNS <s2010060@usls.edu.ph>
2026-04-07 14:17:02 +0800

    buttons

--- src/main/panels/AddBankPanel.java
+++ src/main/panels/AddBankPanel.java
@@ -42,7 +42,41 @@ public class AddBankPanel extends JPanel {
         logoPreview.setBorder(BorderFactory.createLineBorder(Color.GRAY));
         logoPreview.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+        //buttonz
+        JButton chooseBtn= new JButton("Choose Logo");
+        chooseBtn.setBackground(Color.decode("#000000"));
+        chooseBtn.setForeground(Color.decode("#FFFFFF"));
+        chooseBtn.setFocusPainted(false);
+        chooseBtn.setBorderPainted(false);
+        chooseBtn.setOpaque(true);
+        JButton addBtn = new JButton("Add Bank");
+        JButton backBtn = new JButton("Back");
+
+
+        chooseBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        addBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        backBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        chooseBtn.setMaximumSize(new Dimension(180,35));
+        addBtn.setMaximumSize(new Dimension(180,35));
+
+        backBtn.setMaximumSize(new Dimension(100,28));
+
+        addBtn.setBackground(Color.decode("#5D765E"));
+        addBtn.setForeground(Color.WHITE);
+        addBtn.setFocusPainted(false);
+        addBtn.setBorderPainted(false);
+        addBtn.setOpaque(true);
+
+        chooseBtn.addActionListener(e -> chooseLogo());
+        addBtn.addActionListener(e -> addBank());
+        backBtn.addActionListener(e -> mainFrame.showPanel("dashboard"));
+
         
+
+
+
+
 
 
 
